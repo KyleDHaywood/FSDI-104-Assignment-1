50a61665b0e5f9a0cfef7d819a02e274a81ad79b
Kyle <kylehaywood@hotmail.com>
2022-01-10 10:26:12 -0800

    second commit

--- scripts/register.js
+++ scripts/register.js
@@ -12,6 +12,7 @@ let saloon = {
     close: "5:00pm",
   },
   pets: [
+    /*
     {
       name: "Scooby",
       age: 50,
@@ -39,6 +40,7 @@ let saloon = {
       ownerName: "Jeff",
       contactNumber: "579-852-7469",
     },
+    */
   ],
 };
 function Service(shampoo, grooming, meal, play) {
@@ -68,35 +70,103 @@ function PetGenerator(
   this.service = new Service(shampoo, grooming, meal, play);
 }
 
-function getInfo() {
-  let petName = document.getElementById("txtPetName").value;
-  let age = document.getElementById("numAge").value;
-  let gender = document.getElementById("dlGender").value;
-  let breed = document.getElementById("txtBreed").value;
-  let shampoo = document.getElementById("ckShampoo").checked;
-  let grooming = document.getElementById("ckGrooming").checked;
-  let meal = document.getElementById("ckMeal").checked;
-  let play = document.getElementById("ckPlay").checked;
-  let owner = document.getElementById("txtOwner").value;
-  let contact = document.getElementById("txtContactNumber").value;
-  let pet = new PetGenerator(
-    petName,
-    age,
-    gender,
-    breed,
-    owner,
-    contact,
-    shampoo,
-    grooming,
-    meal,
-    play
-  );
-  saloon.pets.push(pet);
-
-  for (let i = 0; i < saloon.pets.length; i++) {
-    console.log(saloon.pets[i]);
+function checkId(variable, id) {
+  if (variable.length < 1) {
+    document.getElementById(id).classList.add("error");
+    return false;
+  } else {
+    document.getElementById(id).classList.remove("error");
+    return true;
   }
-  document.getElementById("petInfo").reset();
+}
+
+function showPetCards() {
+  document.getElementById("petList").innerHTML = ``;
+  for (let i = 0; i < saloon.pets.length; i++) {
+    document.getElementById("petList").innerHTML += createCard(saloon.pets[i]);
+  }
+}
+
+function createCard(pet) {
+  return `<div class="card my-card">
+  <h2>${pet.name}</h2>
+  <label>Age: ${pet.age}</label>
+  <label>Gender: ${pet.gender}</label>
+  <label>Breed: ${pet.breed}</label>
+  <label>Owner: ${pet.owner}</label>
+  <label>Phone: ${pet.contact}</label>
+  <label>${pet.shampoo}</label>
+  <label>${pet.grooming}</label>
+  <label>${pet.meal}</label>
+  <label>${pet.play}</label>
+  <button class="btn btn-danger btn-sm" onclick="removePet(${pet.id})">Delete</button>
+</div>`;
+}
+
+function getInfo() {
+  let isValid = true;
+  let petName = document.getElementById("txtPetName").value;
+  isValid = checkId(petName, "txtPetName");
+  let age = document.getElementById("numAge").value;
+  isValid = checkId(age, "numAge");
+
+  let gender = document.getElementById("dlGender").value;
+  isValid = checkId(gender, "dlGender");
+
+  let breed = document.getElementById("txtBreed").value;
+  isValid = checkId(breed, "txtBreed");
+
+  let shampoo = document.getElementById("ckShampoo").checked;
+  isValid = checkId(shampoo, "ckShampoo");
+
+  let grooming = document.getElementById("ckGrooming").checked;
+  isValid = checkId(grooming, "ckGrooming");
+
+  let meal = document.getElementById("ckMeal").checked;
+  isValid = checkId(meal, "ckMeal");
+
+  let play = document.getElementById("ckPlay").checked;
+  isValid = checkId(play, "ckPlay");
+
+  let owner = document.getElementById("txtOwner").value;
+  isValid = checkId(owner, "txtOwner");
+
+  let contact = document.getElementById("txtContactNumber").value;
+  isValid = checkId(contact, "txtContactNumber");
+
+  if (shampoo || grooming || meal || play) {
+    document.getElementById("ckShampoo").classList.remove("error");
+    document.getElementById("ckGrooming").classList.remove("error");
+    document.getElementById("ckMeal").classList.remove("error");
+    document.getElementById("ckPlay").classList.remove("error");
+  } else {
+    document.getElementById("ckShampoo").classList.add("error");
+    document.getElementById("ckGrooming").classList.add("error");
+    document.getElementById("ckMeal").classList.add("error");
+    document.getElementById("ckPlay").classList.add("error");
+    isValid = false;
+  }
+  if (isValid == true) {
+    let pet = new PetGenerator(
+      petName,
+      age,
+      gender,
+      breed,
+      owner,
+      contact,
+      shampoo,
+      grooming,
+      meal,
+      play
+    );
+    saloon.pets.push(pet);
+
+    for (let i = 0; i < saloon.pets.length; i++) {
+      console.log(saloon.pets[i]);
+    }
+    document.getElementById("petInfo").reset();
+    showPetCards();
+  }
 }
 
 function displayInfo() {}
@@ -120,5 +190,35 @@ function displayNumberOfPets() {
   );
 }
 
-displayAddress();
+function removePet(index) {
+  //search the pet we want to remove
+  saloon.pets.forEach(function callback(pet, value) {
+    if (index === pet.id) {
+      console.log("I found it in the position", value);
+      saloon.pets.splice(value, 1);
+    }
+  });
+  //remove the pet from the array
+  //remove the pet from the html
+  console.log("Removing Pet", index);
+  document.getElementById(index).remove();
+}
+
+function searchPet() {
+  //get the value from the inout search and store it in a variable
+  let searchString = document.getElementById("searchInput").value;
+
+  saloon.pets.forEach(function callback(pet, value) {
+    if (searchString.toLowerCase() === pet.name.toLowerCase()) {
+      document.getElementById(pet.id).classList.add("highlight");
+    }
+  });
+}
+function init() {
+  displayInfo();
+  showPetCards();
+}
+
+window.onload = init;
+
 displayPetNames();
